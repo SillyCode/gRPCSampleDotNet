82ba1111919d5a200ab8ae1a6d6e69b2276eb6d1
SillyCode <SillyCode@users.noreply.github.com>
2023-07-30 12:42:26 +0300

    Initial commit
    Note that there is no solution. 
    Projects must be added on existing solution.

---
+++ grpcClient/Program.cs
@@ -0,0 +1,74 @@
+﻿using Grpc.Net.Client;
+using GrpcEmployee;
+
+//var birthdate = new DateTime(1984, 04, 13);
+//birthdate = DateTime.SpecifyKind(birthdate, DateTimeKind.Utc);
+
+/*
+For testing reading/wrting compiled protobuf
+Employee employee = new()
+{
+    FirstName = "Absalom",
+    LastName = "Smith",
+    BirthDate = Timestamp.FromDateTime(birthdate),
+    MaritalStatus = Employee.Types.MaritalStatus.Married,
+};
+employee.PreviousEmployers.Add("IBM");
+employee.PreviousEmployers.Add("INTEL");
+
+employee.Relatives.Add("Brother", "Morty");
+employee.Relatives.Add("Sister", "Summer");
+
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7042");
+
+
+//For internal testing of protobuf
+using (var output = File.Create("emp.dat"))
+{
+    employee.WriteTo(output);
+}
+
+Employee employeeFromFile;
+using (var input = File.OpenRead("emp.dat"))
+{
+    employeeFromFile = Employee.Parser.ParseFrom(input);
+}
+
+EmployeeId employeeId = new EmployeeId() { Id = 1 };
+
+var client = new EmployeeService.EmployeeServiceClient(channel);
+var reply = client.GetEmployee(employeeId);
+
+Console.WriteLine("GRPC Client");
+*/
+
+EmployeeId employeeId1 = new EmployeeId() { Message = "1" };
+EmployeeId employeeId2 = new EmployeeId() { Message = "2" };
+EmployeeId employeeId3 = new EmployeeId() { Message = "18" };
+
+Console.WriteLine("GRPC Client");
+using var channel = GrpcChannel.ForAddress("http://localhost:5109");
+var client = new Employee.EmployeeClient(channel);
+var reply = client.GetEmployeeById(employeeId1);
+
+Console.WriteLine("Received employee Data");
+Console.WriteLine(reply.FirstName);
+Console.WriteLine("\n");
+
+reply = client.GetEmployeeById(employeeId2);
+
+Console.WriteLine("Received employee Data");
+Console.WriteLine(reply.FirstName);
+Console.WriteLine("\n");
+
+reply = client.GetEmployeeById(employeeId3);
+if (reply != null)
+{
+    Console.WriteLine("Received employee Data");
+    Console.WriteLine(reply.FirstName);
+    Console.WriteLine("\n");
+} else
+{
+    Console.WriteLine("Received employee Data");
+}
---
+++ grpcService/Services/EmployeeService.cs
@@ -0,0 +1,69 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcEmployee;
+
+namespace GrpcService1.Services
+{
+    public class EmployeeService : Employee.EmployeeBase
+    {
+        private List<EmployeeResponse> employees;
+        private readonly ILogger<EmployeeService> _logger;
+        public EmployeeService(ILogger<EmployeeService> logger)
+        {
+            _logger = logger;
+            GenerateEmployees();
+        }
+
+        public override Task<EmployeeResponse> GetEmployeeById(EmployeeId request, ServerCallContext context)
+        {
+            _logger.LogInformation("Received request to: GetEmployeeById");
+            _logger.LogInformation("EmployeeById: " + request);
+
+            EmployeeResponse employeeResponse = new EmployeeResponse();
+            if (request != null) {
+                int id = Convert.ToInt32(request.Message);
+                employeeResponse = GetEmployee(id);
+            }
+            return Task.FromResult(employeeResponse);
+        }
+
+        private EmployeeResponse GetEmployee(int id)
+        {
+            EmployeeResponse employee = employees.FirstOrDefault(x => x.Id == id);
+			if(employee == null) {
+				return new EmployeeResponse();
+			}
+            return employee;
+        }
+
+        private void GenerateEmployees()
+        {
+            employees = new List<EmployeeResponse>();
+            int counter = 5;
+            for (int i = 1; i <= counter; i++)
+            {
+                var birthdate = new DateTime(1984, 04, 13);
+                birthdate = DateTime.SpecifyKind(birthdate, DateTimeKind.Utc);
+
+                var employeeResponse = new EmployeeResponse()
+                {
+                    Id = i,
+                    FirstName = "Absalom" + i,
+                    LastName = "Smith" + i,
+                    BirthDate = Timestamp.FromDateTime(birthdate),
+                    MaritalStatus = EmployeeResponse.Types.MaritalStatus.Married,
+                };
+                employeeResponse.PreviousEmployers.Add("IBM");
+                employeeResponse.PreviousEmployers.Add("INTEL");
+                employeeResponse.CurrentAddress = new Address()
+                {
+                    City = "Hiafa",
+                    StreetName = "Herzel",
+                    HouseNumber = 5,
+                    ZipCode = "123455",
+                };
+                employees.Add(employeeResponse);
+            }
+        }
+    }
+}
